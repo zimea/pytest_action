236064393d02e335e436e4213182d79914cfd01e
zimea <lea.zimmermann@online.de>
2022-08-22 17:46:58 +0200

    feat: add test files

---
+++ create_venv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR='venv'
+#FILE='requirements.txt'
+
+python3 -m venv $DIR
+source $DIR/bin/activate
+#pip3 install -r $FILE
+#pip3 install jupyterlab ipykernel
+#python3 -m ipykernel install --user --name=$DIR
---
+++ test_sample.py
@@ -0,0 +1,7 @@
+import pytest
+
+def func(x):
+    return x + 2
+
+def test_success():
+    assert func(3) == 5
